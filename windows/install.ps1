bfb68cad5db382607b3c2ac6025bb943f0f95316
Carlos Henrique Prado Sousa <chpsousa@hotmail.com>
2020-09-24 17:52:11 -0300

    adding windows ps1 script

--- windows/install.ps1
+++ windows/install.ps1
@@ -4,6 +4,18 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# And Install Choco Packages
+choco install googlechrome
+choco install git
+choco install steam
+choco install vscode
+choco install docker-desktop
+choco install firacode
+choco install visualstudio2019professional
+choco install istioctl
+choco install telegram
+choco install lens
+
 # Install PowerShell Modules
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
